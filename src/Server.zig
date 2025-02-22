dbbcf7c9b9da109bc7e3bfe8c3e0ad515877e0ca
Nhân <hi@imnhan.com>
2025-02-22 18:06:20 +0700

    open db conn in the same thread that it's used

--- src/Server.zig
+++ src/Server.zig
@@ -11,21 +11,26 @@ const Server = @This();
 gpa: mem.Allocator = undefined,
 address: net.Address = undefined,
 net_server: net.Server = undefined,
-conn: zqlite.Conn = undefined,
+file_path: [:0]const u8 = undefined,
 
 pub fn init(gpa: mem.Allocator, port: u16, file_path: [:0]const u8) !*Server {
     var server = try gpa.create(Server);
     server.gpa = gpa;
     server.address = try net.Address.parseIp4("127.0.0.1", port);
     server.net_server = try server.address.listen(.{ .reuse_address = true });
-    server.conn = try zqlite.Conn.init(file_path, zqlite.OpenFlags.EXResCode | zqlite.OpenFlags.ReadOnly);
+    server.file_path = try gpa.dupeZ(u8, file_path);
 
-    // TODO: creating an sqlite connection in one thread then using it in
-    // another is probably bad. Is there a better way?
     var thread = try std.Thread.spawn(.{}, start_server, .{server});
     thread.detach();
 
     return server;
+}
+
+pub fn deinit(self: *Server) void {
+    self.gpa.free(self.file_path);
+    self.net_server.deinit();
+    self.gpa.destroy(self);
+    print("Server.deinit() done.\n", .{});
 }
 
 fn start_server(self: *Server) void {
@@ -33,7 +38,7 @@ fn start_server(self: *Server) void {
 
     while (true) {
         var connection = net_server.accept() catch |err| {
-            std.debug.print("Connection to client interrupted: {}\n", .{err});
+            print("Connection to client interrupted: {}\n", .{err});
             continue;
         };
         defer connection.stream.close();
@@ -42,29 +47,32 @@ fn start_server(self: *Server) void {
         var http_server = http.Server.init(connection, &read_buffer);
 
         var request = http_server.receiveHead() catch |err| {
-            std.debug.print("Could not read head: {}\n", .{err});
+            print("Could not read head: {}\n", .{err});
             continue;
         };
-        self.handle_request(&request) catch |err| {
-            std.debug.print("Could not handle request: {}", .{err});
+
+        var conn = zqlite.Conn.init(
+            self.file_path,
+            zqlite.OpenFlags.EXResCode | zqlite.OpenFlags.ReadOnly,
+        ) catch |err| {
+            print("Could not open db: {}\n", .{err});
+            continue;
+        };
+        defer conn.close();
+
+        handle_request(&request, conn) catch |err| {
+            print("Could not handle request: {}", .{err});
             continue;
         };
     }
 }
 
-fn handle_request(self: *Server, request: *http.Server.Request) !void {
-    std.debug.print("Handling request for {s}\n", .{request.head.target});
+fn handle_request(request: *http.Server.Request, conn: zqlite.Conn) !void {
+    print("Handling request for {s}\n", .{request.head.target});
 
     // very dumb code just to confirm db connection works
     const id = try std.fmt.parseInt(i64, request.head.target["/".len..], 10);
-    var row = try sql.selectRow(self.conn, "select title from post where id=?", .{id});
+    var row = try sql.selectRow(conn, "select title from post where id=?", .{id});
     defer row.?.deinit();
     try request.respond(row.?.text(0), .{});
-}
-
-pub fn deinit(self: *Server) void {
-    self.conn.close();
-    self.net_server.deinit();
-    self.gpa.destroy(self);
-    print("Server.deinit() done.\n", .{});
 }
