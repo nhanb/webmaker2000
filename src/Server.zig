00326b6842e833b05a074f123f9d000760fae53c
Nhân <hi@imnhan.com>
2025-02-23 09:35:32 +0700

    send shutdown request via http like a true webshit

--- src/Server.zig
+++ src/Server.zig
@@ -8,23 +8,24 @@ const sql = @import("sql.zig");
 
 const Server = @This();
 
-gpa: mem.Allocator = undefined,
-address: net.Address = undefined,
-net_server: net.Server = undefined,
-file_path: [:0]const u8 = undefined,
-
+gpa: mem.Allocator,
+address: net.Address,
+net_server: net.Server,
+file_path: [:0]const u8,
 thread: std.Thread,
-running: bool = true,
-running_mut: std.Thread.Mutex = .{},
+
+/// Send an http request to this path to shut down the server.
+const SHUTDOWN_PATH = "/_shutdown";
 
 pub fn init(gpa: mem.Allocator, port: u16, file_path: [:0]const u8) !*Server {
+    print("Server starting at port {d}\n", .{port});
+
     var server = try gpa.create(Server);
     server.* = .{
         .gpa = gpa,
         .address = try net.Address.parseIp4("127.0.0.1", port),
         .net_server = try server.address.listen(.{ .reuse_address = true }),
         .file_path = try gpa.dupeZ(u8, file_path),
-        .running = true,
         .thread = try std.Thread.spawn(.{}, start_server, .{server}),
     };
 
@@ -32,30 +33,35 @@ pub fn init(gpa: mem.Allocator, port: u16, file_path: [:0]const u8) !*Server {
 }
 
 pub fn deinit(self: *Server) void {
-    print("Server.deinit() starting\n", .{});
-    self.running_mut.lock();
-    self.running = false;
-    self.running_mut.unlock();
+    {
+        // Send shutdown request
+        var client = std.http.Client{ .allocator = self.gpa };
+        defer client.deinit();
+        _ = client.fetch(.{
+            .method = .GET,
+            .location = .{
+                .uri = .{
+                    .scheme = "http",
+                    .host = .{ .raw = "127.0.0.1" },
+                    .path = .{ .raw = SHUTDOWN_PATH },
+                    .port = self.net_server.listen_address.getPort(),
+                },
+            },
+        }) catch |err| {
+            print("Failed to send shutdown request: {}\n", .{err});
+            unreachable;
+        };
+    }
 
     self.thread.join();
     self.gpa.free(self.file_path);
     self.net_server.deinit();
     self.gpa.destroy(self);
-    print("Server.deinit() done.\n", .{});
+    print("Server shut down cleanly.\n", .{});
 }
 
 fn start_server(self: *Server) void {
-    print("Starting server\n", .{});
-
-    var running = true;
-
-    while (running) {
-        self.running_mut.lock();
-        running = self.running;
-        self.running_mut.unlock();
-
-        print("running = {}\n", .{running});
-
+    while (true) {
         var connection = self.net_server.accept() catch |err| {
             print("Connection to client interrupted: {}\n", .{err});
             continue;
@@ -69,6 +75,11 @@ fn start_server(self: *Server) void {
             print("Could not read head: {}\n", .{err});
             continue;
         };
+
+        if (std.mem.eql(u8, request.head.target, SHUTDOWN_PATH)) {
+            request.respond("bye", .{}) catch unreachable;
+            break;
+        }
 
         var conn = zqlite.Conn.init(
             self.file_path,
@@ -87,11 +98,15 @@ fn start_server(self: *Server) void {
 }
 
 fn handle_request(request: *http.Server.Request, conn: zqlite.Conn) !void {
-    print("Handling request for {s}\n", .{request.head.target});
+    print("Server serving {s}\n", .{request.head.target});
 
     // very dumb code just to confirm db connection works
     const id = try std.fmt.parseInt(i64, request.head.target["/".len..], 10);
-    var row = try sql.selectRow(conn, "select title from post where id=?", .{id});
-    defer row.?.deinit();
-    try request.respond(row.?.text(0), .{});
+    const row = try sql.selectRow(conn, "select title from post where id=?", .{id});
+    if (row) |r| {
+        defer r.deinit();
+        try request.respond(r.text(0), .{});
+    } else {
+        try request.respond("nope", .{});
+    }
 }
